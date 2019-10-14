6e3143a9822d7807cbdc0e0bed32fc1491cb035b
Pufcor <mis.robert1@gmail.com>
2019-10-14 20:36:17 +0200

    add function print_all_friends()

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 my_friends = ["Daniel", "costa", "myszka"]
 
-for XD in my_friends:
-    print("My friend to: " + XD)
-    print("--------------------")
+
+def print_all_friends():
+    for XD in my_friends:
+        print("*******")
+        print("My friend to: " + XD)
+        print("--------------------")
+
+
+print_all_friends()
 
 imie = input("podaj imię psyjacielu: ")
 
 my_friends.append(imie)
 
-for XD in my_friends:
-    print("My friend to: " + XD)
-    print("--------------------")
-
+print_all_friends()
