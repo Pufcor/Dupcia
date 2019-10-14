eecf17dc76cc18e8e8f69925ad0c8c1ba03702cb
Pufcor <mis.robert1@gmail.com>
2019-10-14 21:01:53 +0200

    Added removing enemies

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ def print_all_friends():
 
 
 print_all_friends()
-
-name = input("podaj imię psyjacielu: ")
-
-my_friends.append(name)
-
-print_all_friends()
+while True:
+    friend = input("Którego frienda już nie lubish????? ")
+    if friend == "koniec":
+        break
+    if len(my_friends) == 0:
+        print("Nie masz już wrogów")
+        break
+    else:
+        if friend in my_friends:
+            my_friends.remove(friend)
+            print_all_friends()
+        else:
+            print("Nie mam takiego ziomka")
