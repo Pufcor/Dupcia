9a492898b2cb277d58eae7a0b6edb8a94409d1d7
Pufcor <mis.robert1@gmail.com>
2020-06-10 22:11:05 +0200

    dodanie slowneloczby.py

---
+++ slowneliczby.py
@@ -0,0 +1,6 @@
+liczby = ["zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć"]
+liczba = "dwa"
+
+liczby.index("dwa")
+print(liczby.index("dwa"))
+print(liczby.index("dziewięć"))
