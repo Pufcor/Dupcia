a5e9d764ae90ad139c68afba430150dec760a74b
Pufcor <mis.robert1@gmail.com>
2020-06-12 22:04:39 +0200

    Utworzenie listy kolorów, wraz z listą pomysłów

---
+++ bazakolorow.py
@@ -0,0 +1,36 @@
+bk = []
+
+k1 = input("podaj kolor ")
+bk.append(k1)
+k2 = input("podaj drugi kolor ")
+bk.append(k2)
+a1 = input("aby dodać kolejny kolor, wpisz <<dalej>>. W przeciwnym wypadku wpisz x ")
+if "dalej" in a1:
+    k3 = input("podaj trzeci kolor ")
+    bk.append(k3)
+else:
+    a2 = input("czy chcesz sprawdzić listę kolorów?")
+    if a2 == "tak" or "TAK" or "Tak" or 'ok':
+        print(bk)
+    if a2 == "nie" or "Nie" or "NIE":
+        print("szkoda")
+
+
+
+
+
+<zadać pytanie ile ma być kolorów>
+//dodać opcję pokazania listy na każdym etapie po wpisaniu komendy LISTA
+//stworzyc drugi, osobny plik
+//w drugim pliku polecenie na użytkownika ma być stworzenie listy ubrań - program ma sam podpowiedzić ile
+a liczba ta ma być zależna od liczby kolorów w pliku 1
+//utowrzyć plik, który po wpisaniu k1 + u1 ma tworzyć komendę niebieski płaszcz
+
+
+
+
+
+
+
+
+
